b4f661d9e1526b3cdfbf867f5b1e1db058a28c1e
themodelhq <themodelhq@gmail.com>
2026-04-21 19:00:46 +0100

    Delete drizzle directory

--- drizzle/0002_add_password_hash.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add passwordHash column and make email unique
-ALTER TABLE `users`
-  ADD COLUMN `passwordHash` varchar(255) DEFAULT NULL AFTER `email`;
-
-ALTER TABLE `users`
-  ADD UNIQUE INDEX `users_email_unique` (`email`);
